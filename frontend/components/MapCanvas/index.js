d65ffc277d8e38601c43f78989a5e8030a4e07cc
Yuxiang Zhu <yuxiangzhu@Yuxiangs-MacBook-Pro.local>
2019-07-16 20:43:43 +0800

    scaling

--- frontend/components/MapCanvas/index.js
+++ frontend/components/MapCanvas/index.js
@@ -1,17 +1,48 @@
 import React from 'react';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
+import { Dimensions } from 'react-native';
 import allReducers from '../../reducer';
 import MapCanvas from './mapCanvas';
-import { Button } from 'react-native';
+import Animated from "react-native-reanimated";
+import { PanGestureHandler, State } from "react-native-gesture-handler";
+
+const {Value, event, cond, eq, add, set, interpolate} = Animated;
+
 const store = createStore(allReducers);
 
+const { width, height } = Dimensions.get("window");
+
 export default class MapCanvasWrapper extends React.Component{
+    dragX = new Value(0);
+    dragY = new Value(0);
+    offsetX = new Value(width/2);
+    offsetY = new Value(height/2);
+    gestureState = new Value(-1);
+    onGestureEvent = event([
+        {
+          nativeEvent: {
+            translationX: this.dragX,
+            translationY: this.dragY,
+            state: this.gestureState
+          }
+        }
+    ]);
+    transX = cond(
+        eq(this.gestureState, State.ACTIVE),
+        add(this.offsetX, this.dragX),
+        set(this.offsetX, add(this.offsetX, this.dragX)),
+      );
+    
+      transY = cond(
+        eq(this.gestureState, State.ACTIVE),
+        add(this.offsetY, this.dragY),
+        set(this.offsetY, add(this.offsetY, this.dragY)),
+    );
 
     render(){
         return (
             <Provider store={store}>
-                <Button title='Test' onPress={()=>{console.log(store.getState())}}/>
                 <MapCanvas />
             </Provider>
         );
