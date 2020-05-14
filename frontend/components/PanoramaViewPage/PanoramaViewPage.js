18c4747c0b69667efea35057c03e0985eab53bec
samor412 <klor@connect.ust.hk>
2020-05-14 21:47:04 +0800

    bugFix: android production can't load http image
    https://stackoverflow.com/questions/56012702/react-native-cant-show-remote-images-in-release-mode-on-android-device/56013183

--- frontend/components/PanoramaViewPage/PanoramaViewPage.js
+++ frontend/components/PanoramaViewPage/PanoramaViewPage.js
@@ -21,7 +21,7 @@ export default class PanoramaViewPage extends React.Component {
                     style={styles.viewer}
                     dimensions={{ height: Dimensions.get('window').height, width: Dimensions.get("window").width }}
                     inputType="mono"
-                    imageUrl="http://klor.student.ust.hk/image/panorama_sample.jpg"
+                    imageUrl="https://i.ytimg.com/vi/yspFy7_oEKk/hqdefault.jpg"
                 />
             </View>
         );
